0075cc181f8476d8284ea3c82079ffe9affe1b17
nikiljos <57913645+nikiljos@users.noreply.github.com>
2021-05-31 22:16:17 +0530

    first working commit

---
+++ cowin.js
@@ -0,0 +1,117 @@
+function vaccineResult() {
+
+  pin = document.getElementById('pin').value
+
+  console.log(pin)
+
+  document.getElementById('res').innerHTML = ""
+
+
+
+
+  // console.log(district)
+
+
+  // var result = ""
+
+  var tCap = 0
+
+
+  let centersArray = new Array;
+
+
+
+  let date_ob = new Date();
+
+  // current date
+  // adjust 0 before single digit date
+  let date = ("0" + date_ob.getDate()).slice(-2);
+
+  // current month
+  let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+  // current year
+  let year = date_ob.getFullYear();
+
+  var formattedDate = date + "-" + month + "-" + year;
+
+  console.log(formattedDate)
+
+  url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=" + pin + "&date=" + formattedDate
+
+  axios.get(url)
+    .then(function(response) {
+      cowinData = response.data
+
+
+
+      function length(jsonData) {
+        return Object.keys(jsonData).length;
+      }
+
+      // x = length(cowinData.centres);
+
+
+      // console.log(x)
+
+      // dName = cowinData.sessions[0].district_name
+
+      var availableCap = 0;
+
+      // if (resError == 1) {
+      //   let er = "<div > Sorry, Seems like some error occured.Pls try again < /div>"
+      // }
+
+      if (cowinData != "") {
+
+
+
+
+
+
+        y = cowinData.sessions.length
+
+        for (n = 0; n < y; n++) {
+
+          var cap = cowinData.sessions[n].available_capacity;
+          availableCap = availableCap + cap
+
+          if (cap > 0) {
+
+            let divCode = "<div class='col-md-4'><div class='card mb-4 box-shadow'><div class='card-body'> <p class='card-text'><b>Center:</b> " + cowinData.sessions[n].name + "<br> <b>Vaccine Name:</b>" + cowinData.sessions[n].vaccine + "<br> <b>Date:</b> " + cowinData.sessions[n].date + " <br> <b>Slots Available:</b> " + cowinData.sessions[n].available_capacity + "<br> <b>Mininum Age:</b> " + cowinData.sessions[n].min_age_limit + "<br> </p> <small class='text-muted'><a href='https://selfregistration.cowin.gov.in/'>Book your slot</a></small></div> </div> </div>"
+            document.getElementById('res').innerHTML += divCode
+          }
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+        // console.log("Kot?tayam")
+
+        centersArray.push("Availble slots: " + availableCap)
+
+        centersArray.push("Total centres listed: " + x)
+
+        tCap = tCap + availableCap;
+
+        console.log(centersArray)
+
+
+      }
+    })
+    .catch(function(error) {
+      // handle error
+      // console.log(error);
+      console.log("error in response", error)
+      resError = 1;
+
+    })
+}
