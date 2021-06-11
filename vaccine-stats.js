39f92361f257b81ccc5cdcfea7c9544f3eb18526
nikiljos <57913645+nikiljos@users.noreply.github.com>
2021-06-11 21:16:30 +0530

    ...

--- vaccine-stats.js
+++ vaccine-stats.js
@@ -10,7 +10,7 @@ function vaccineStats() {
         // document.getElementById('vData').innerHTML = "<b>" + vacData.summary.tot_person_vaccinations + "</b> citizens have already took their first dose in Kerala.<br>Be a smart citizen and take your shot as soon as possible."
       document.getElementById('kTotalD1').innerHTML = vacData.summary.tot_person_vaccinations
       document.getElementById('kTotalD2').innerHTML = vacData.summary.second_dose
-      document.getElementById('totalShots').innerHTML = vacData.summary.tot_vaccinations
+      document.getElementById('totalShots').innerHTML = vacData.summary.tot_person_vaccinations + vacData.summary.second_dose
       document.getElementById('lastUpdate').innerHTML = vacData.last_updated
     })
 }
